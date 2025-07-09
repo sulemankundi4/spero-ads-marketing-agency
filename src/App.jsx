c1b173cd2bb3889c5147e40d7e44263f495749d3
sulemankundi4 <msulemankdi@gmail.com>
2025-07-09 17:00:53 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <div className="text-center text-2xl font-bold">You will see progress here</div>
+
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
